3d1222197ecdb6e724d50e52589b12f6d3ea5322
martijnl00 <martijn.langeweg@outlook.com>
2022-07-20 14:45:28 +0200

    changes in .rst files

--- docs/conf.py
+++ docs/conf.py
@@ -105,10 +105,10 @@ htmlhelp_basename = 'autodoc'
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
-man_pages = [
-    (master_doc, 'helloworld', 'helloWorld Documentation',
-     [author], 1)
-]
+# man_pages = [
+#     (master_doc, 'helloworld', 'helloWorld Documentation',
+#      [author], 1)
+# ]
 
 # -- Extension configuration -------------------------------------------------
  
