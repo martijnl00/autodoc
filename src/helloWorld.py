29628bb7046307aa69aef23ec8b6cf3f3fad12be
martijnl00 <martijn.langeweg@outlook.com>
2022-07-26 09:21:08 +0200

    imports added

--- src/helloWorld.py
+++ src/helloWorld.py
@@ -1,4 +1,9 @@
-from some_functions.my_funcs import send_mail_v2
+import urllib3
+import psycopg2
+import idna
+import requests
+import some_functions
+# from some_functions.my_funcs import send_mail_v2
 
 """This is a very simple helloWorld python script"""
 def helloWorld( msg ):
